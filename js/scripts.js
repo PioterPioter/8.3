67c56b7217fc807cdbc186f5b090a156e44dfa57
Iwona Ostaszewska <iwonaostaszewska@MacBook-Air-Iwona.local>
2017-03-01 10:02:49 -0500

    add

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+/*
+  komentarz blokowy
+  console.log(‘Zakomentowana się nie wywołam’);
+*/
+var name = prompt('piotr');
+// komentarz liniowy
+alert('Witaj, ' + name);
+/*
+  komentarz blokowy
+  console.log(‘Zakomentowana się nie wywołam’);
+*/
+console.log('Witaj, ' + name);
